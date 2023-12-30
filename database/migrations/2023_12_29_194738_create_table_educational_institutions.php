8901a964a437461df1f7521c702e8db3a187fdba
Thomas Hladki <tomhladki@icloud.com>
2023-12-30 02:44:43 -0500

    First 2 migrations

---
+++ database/migrations/2023_12_29_194738_create_table_educational_institutions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('educational_institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('medical_school_name')->unique();
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('province');
+            $table->string('postal_code');
+            $table->string('email_domain')->unique();
+
+            $table->timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('educational_institutions', function (Blueprint $table) {
+            
+        });
+    }
+};
