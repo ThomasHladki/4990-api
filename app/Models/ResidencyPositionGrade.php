9d84d4a44bbcb904f809d295aeb0de2091e6e25c
Thomas Hladki <tomhladki@icloud.com>
2024-01-13 23:35:29 -0500

    Initial Models

---
+++ app/Models/ResidencyPositionGrade.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResidencyPositionGrade extends Model
+{
+    use HasFactory;
+}
